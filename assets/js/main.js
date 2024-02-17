81ae22534e02ac901c820efd11394756cc4c80d0
CrisContrerasC <cristina.contreras.celedon@gmail.com>
2024-02-17 11:42:51 -0300

    Se configura Tooltips y Popover en main.js y se agrega tooltip en html boton enviar de formulario de contacto

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+$(function(){
+    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+    const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+    //Configuración de tooltips
+
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+    
+
+    // Tooltip
+    $('#enviar_formulario').attr('title', 'Presiona para enviar formulario');
+    $('[data-bs-toggle="tooltip"]').tooltip();
+
+})
